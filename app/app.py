b07ebc9ea3d3c8c4d65d2b28b9de51fb34148811
kaedemalu <kaedemalu@gmail.com>
2024-03-20 16:55:51 +0900

    Initial Commit

---
+++ app/app.py
@@ -0,0 +1,123 @@
+import os
+from dotenv import load_dotenv
+from fastapi import FastAPI, APIRouter, Request
+from slack_bolt import App
+from slack_bolt.adapter.fastapi import SlackRequestHandler
+
+from slack_sdk.web.async_client import AsyncWebClient
+
+import vertexai
+from vertexai.language_models import TextGenerationModel
+
+load_dotenv()
+
+try:
+    project_id = os.environ['PROJECT_ID']
+    region = os.environ['REGION']
+    slack_bot_token = os.environ['SLACK_BOT_TOKEN']
+    slack_signing_secret = os.environ['SLACK_SIGNING_SECRET']
+except KeyError as e:
+    sys.exit(f"Environment variable not set: {e}")
+
+app = App(token=slack_bot_token, signing_secret=slack_signing_secret)
+app_handler = SlackRequestHandler(app)
+api = FastAPI(
+    docs_url = "/"
+)
+router = APIRouter()
+
+@api.get('/health')
+async def health():
+  response = {
+    'status': 'up'
+  }
+  return response
+
+@api.post('/slack/events')
+async def events(req: Request):
+    return await app_handler.handle(req)
+
+# VertexAIを初期化
+vertexai.init(project=project_id, location=region)
+
+text_model = TextGenerationModel.from_pretrained("text-bison")
+PARAMETERS = {
+    "max_output_tokens": 1024,
+    "temperature": 0.20,
+    "top_p": 0.95,
+    "top_k": 40,
+}
+
+RESPONSE_STYLE = """"""
+
+# cloud logging
+logging_client = logging.Client()
+
+# cloud logging: 書き込むログの名前
+logger_name = "palm2_slack_chatbot"
+
+# cloud logging: ロガーを選択する
+logger = logging_client.logger(logger_name)
+
+# 本動作はここから
+
+
+def generate_response(
+    client: AsyncWebClient,
+    ts: str,
+    conversation_thread: str,
+    user_id: str,
+    channel_id: str,
+    prompt: str,
+) -> None:
+    """
+    ユーザーIDがボットのIDまたはNoneでなく、かつチャンネルIDが存在する場合、Slackチャンネルにメッセージを投稿する。
+
+    Parameters
+    ----------
+    ts : str
+        メッセージのタイムスタンプ
+    user_id : str
+        ユーザーID
+    channel_id : str
+        チャンネルID
+    prompt : str
+        プロンプト
+    """
+    response = text_model.predict(prompt, **PARAMETERS)
+
+    # ブロックされたか確認する
+    is_blocked = response.is_blocked
+    is_empty_response = len(response.text.strip(" \n")) < 1
+
+    if is_blocked or is_empty_response:
+        payload = "入力または出力が Google のポリシーに違反している可能性があるため、出力がブロックされています。プロンプトの言い換えや、パラメータ設定の調整を試してください。"
+    else:
+        # slackで**などのmarkdownを正しく表示できないので削除し、簡潔にする
+        payload = utils.remove_markdown(response.text)
+
+    # レスポンスをslackへ返す
+    client.chat_postMessage(channel=channel_id, thread_ts=ts, text=payload)
+
+    keyword = gc_utils.get_keyword(text_model, prompt, PARAMETERS)
+    gc_utils.send_log(logger, user_id, prompt, payload, keyword)
+
+
+@app.event("message")
+def handle_incoming_message(client: AsyncWebClient, payload: dict) -> None:
+    """
+    受信メッセージを処理する
+
+    Parameters
+    ----------
+    payload : dict
+        ペイロード
+    """
+    channel_id = payload.get("channel")
+    user_id = payload.get("user")
+    prompt = payload.get("text")
+    ts = payload.get("ts")
+    thread_ts = payload.get("thread_ts")
+    conversation_thread = ts if thread_ts is None else thread_ts
+    generate_response(client, ts, conversation_thread,
+                      user_id, channel_id, prompt)
