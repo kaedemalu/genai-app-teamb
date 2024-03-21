c808c325fec81884a6b92620cf9880dacabc8d07
ggen-jkamiya <jkamiya@g-gen.co.jp>
2024-03-21 15:43:36 +0900

    update

--- app/app.py
+++ app/app.py
@@ -19,6 +19,7 @@ from google.api_core.client_options import ClientOptions
 from google.cloud import discoveryengine_v1 as discoveryengine
 from google.protobuf import json_format
 import json
+import pprint
 
 load_dotenv()
 
@@ -78,49 +79,6 @@ logger_name = "palm2_slack_chatbot"
 # cloud logging: ロガーを選択する
 logger = logging_client.logger(logger_name)
 
-# 本動作はここから
-
-
-def generate_response(
-    client: AsyncWebClient,
-    ts: str,
-    conversation_thread: str,
-    user_id: str,
-    channel_id: str,
-    prompt: str,
-) -> None:
-    """
-    ユーザーIDがボットのIDまたはNoneでなく、かつチャンネルIDが存在する場合、Slackチャンネルにメッセージを投稿する。
-
-    Parameters
-    ----------
-    ts : str
-        メッセージのタイムスタンプ
-    user_id : str
-        ユーザーID
-    channel_id : str
-        チャンネルID
-    prompt : str
-        プロンプト
-    """
-    response = text_model.predict(prompt, **PARAMETERS)
-
-    # ブロックされたか確認する
-    is_blocked = response.is_blocked
-    is_empty_response = len(response.text.strip(" \n")) < 1
-
-    if is_blocked or is_empty_response:
-        payload = "入力または出力が Google のポリシーに違反している可能性があるため、出力がブロックされています。プロンプトの言い換えや、パラメータ設定の調整を試してください。"
-    else:
-        # slackで**などのmarkdownを正しく表示できないので削除し、簡潔にする
-        payload = remove_markdown(response.text)
-
-    # レスポンスをslackへ返す
-    client.chat_postMessage(channel=channel_id, thread_ts=ts, text=payload)
-
-    keyword = get_keyword(text_model, prompt, PARAMETERS)
-    send_log(logger, user_id, prompt, payload, keyword)
-
 
 def generate_response_by_vertex_ai_search(
     client: AsyncWebClient,
@@ -147,109 +105,52 @@ def generate_response_by_vertex_ai_search(
     response = search_sample(project_id=project_id,
                              location=vertex_ai_search_location,
                              engine_id=engine_id,
-                             search_query=prompt)
+                             search_query=prompt,
+                             user_id=user_id)
 
     # レスポンスをslackへ返す
     client.chat_postMessage(channel=channel_id, thread_ts=ts,
                             text=response)
 
 
-def send_log(logger, user_id: str, prompt: str, payload: str, keyword: str) -> None:
-    """
-    ログを送信する
+# def send_log(logger, user_id: str, prompt: str, payload: str, keyword: str) -> None:
+#     """
+#     ログを送信する
 
-    Parameters
-    ----------
-    user_id : str
-        ユーザーID
-    prompt : str
-        プロンプト
-    payload : str
-        ペイロード
-    """
+#     Parameters
+#     ----------
+#     user_id : str
+#         ユーザーID
+#     prompt : str
+#         プロンプト
+#     payload : str
+#         ペイロード
+#     """
 
-    # ログに書き込むデータを持つ辞書を作成する
-    data = {
-        "slack_user_id": user_id,
-        "prompt": prompt,
-        "response": payload,
-        "keyword": keyword,
-    }
+#     # ログに書き込むデータを持つ辞書を作成する
+#     data = {
+#         "slack_user_id": user_id,
+#         "prompt": prompt,
+#         "response": payload,
+#         "keyword": keyword,
+#     }
 
-    # 辞書をJSON文字列に変換する
-    logger.log_struct(data)
-
-
-def get_keyword(text_model, prompt: str, parameters) -> str:
-    """
-    キーワードを取得する
-
-    Parameters
-    ----------
-    prompt : str
-        プロンプト
-
-    Returns
-    ----------
-    str
-        キーワード
-    """
-    get_keyword_prompt = f"""勉強会の計画
-
-    以下の文章からテーマまたコンテキストとなるキーワードを1つ生成してください: サボテンの育て方
-    コンテンツの消費者は、サボテンの育て方に興味がある人々
-    サボテンは、初心者でも育てやすい植物です。しかし、サボテンを健康に育てるためには、いくつかの注意点があります。
-    サボテンの育て方について、以下の点を教えてください。
-    - サボテンの種類
-    - サボテンの土
-    - サボテンの水やり
-    - サボテンの肥料
-    - サボテンの剪定
-    - サボテンの病気と害虫
-    テーマまたコンテキストとなるキーワード: サボテンの育て方
-
-    以下の文章からテーマまたコンテキストとなるキーワードを1つ生成してください: 鍼灸学において、よだれつわりの治療法を教えてください。また、理由と考え方も教えてください。読み手は鍼灸師だとします。鍼を打つべきなツボとその理由を詳しく教えてください
-    テーマまたコンテキストとなるキーワード: 鍼灸学医療
-
-    以下の文章からテーマまたコンテキストとなるキーワードを1つ生成してください: 金融業界のビジネスプラン
-    金融業界のビジネスプランを作成してください。
-    ビジネスプランには、以下の項目を含めてください。
-    - 会社の概要
-    - 製品やサービスの概要
-    - 市場分析
-    - 競合分析
-    - マーケティング戦略
-    - 財務計画
-    ビジネスプランは、3年間で利益を出すことを目標としてください。
-    テーマまたコンテキストとなるキーワード: ビジネスプランの作成
-
-    以下の文章からテーマまたコンテキストとなるキーワードを1つ生成してください: 30代男性向けのマーケティングのアイディアを教えてください
-    テーマまたコンテキストとなるキーワード: マーケティング戦略
-
-    以下の文章からテーマまたコンテキストとなるキーワードを1つ生成してください: {prompt}
-    テーマまたコンテキストとなるキーワード:
-    """
-    response = text_model.predict(get_keyword_prompt, **parameters)
-    is_blocked = response.is_blocked
-    if is_blocked:
-        keyword = "入力または出力が Google のポリシーに違反している可能性があるため、出力がブロックされています。プロンプトの言い換えや、パラメータ設定の調整を試してください。現在、英語にのみ対応しています。"
-    else:
-        keyword = remove_markdown(response.text)
-    return keyword
+#     # 辞書をJSON文字列に変換する
+#     logger.log_struct(data)
 
 
-def remove_markdown(text):
-    # インラインコードブロックを削除する
-    text = re.sub(r"`(.+?)`", r"\1", text)
-    # マルチラインコードブロックを削除する
-    text = re.sub(r"```(.+?)```", r"\1", text)
-    # ボールドマークダウンを削除する
-    text = re.sub(r"\*\*(.+?)\*\*", r"\1", text)
-    # イタリックマークダウンを削除する
-    text = re.sub(r"\*(.+?)\*", r"\1", text)
-    # ヘッダーマークダウンを---で置き換える
-    text = re.sub(r"^#{1,6}\s*(.+)", r"---\1---", text, flags=re.MULTILINE)
-    return text.replace("[Example]:", "")
+# def remove_markdown(text):
+#     # インラインコードブロックを削除する
+#     text = re.sub(r"`(.+?)`", r"\1", text)
+#     # マルチラインコードブロックを削除する
+#     text = re.sub(r"```(.+?)```", r"\1", text)
+#     # ボールドマークダウンを削除する
+#     text = re.sub(r"\*\*(.+?)\*\*", r"\1", text)
+#     # イタリックマークダウンを削除する
+#     text = re.sub(r"\*(.+?)\*", r"\1", text)
+#     # ヘッダーマークダウンを---で置き換える
+#     text = re.sub(r"^#{1,6}\s*(.+)", r"---\1---", text, flags=re.MULTILINE)
+#     return text.replace("[Example]:", "")
 
 
 # @app.event("message")
@@ -278,6 +179,8 @@ def search_sample(
     location: str,
     engine_id: str,
     search_query: str,
+    user_id: str = None
+
 ) -> List[discoveryengine.SearchResponse]:
 
     client_options = (
@@ -300,22 +203,23 @@ def search_sample(
         # For information about snippets, refer to:
         # https://cloud.google.com/generative-ai-app-builder/docs/snippets
         snippet_spec=discoveryengine.SearchRequest.ContentSearchSpec.SnippetSpec(
-            return_snippet=False
+            return_snippet=True
         ),
         # For information about search summaries, refer to:
         # https://cloud.google.com/generative-ai-app-builder/docs/get-search-summaries
         summary_spec=discoveryengine.SearchRequest.ContentSearchSpec.SummarySpec(
             summary_result_count=5,
-            include_citations=False,
+            include_citations=True,
             ignore_adversarial_query=True,
             ignore_non_summary_seeking_query=True,
-            model_prompt_spec=discoveryengine.SearchRequest.ContentSearchSpec.SummarySpec.ModelPromptSpec(
-                preamble="YOUR_CUSTOM_PROMPT"
-            ),
+            # model_prompt_spec=discoveryengine.SearchRequest.ContentSearchSpec.SummarySpec.ModelPromptSpec(
+            #     preamble="YOUR_CUSTOM_PROMPT"
+            # ),
             model_spec=discoveryengine.SearchRequest.ContentSearchSpec.SummarySpec.ModelSpec(
                 version="gemini-1.0-pro-001/answer_gen/v1",
+                # version="preview",
             ),
-        ),
+        )
     )
 
     # Refer to the `SearchRequest` reference for all supported fields:
@@ -331,9 +235,12 @@ def search_sample(
         spell_correction_spec=discoveryengine.SearchRequest.SpellCorrectionSpec(
             mode=discoveryengine.SearchRequest.SpellCorrectionSpec.Mode.AUTO
         ),
+        user_pseudo_id=user_id
     )
 
     response = client.search(request)
+    response_ = MessageToDict(response._pb)
+    pprint.pprint(response_)
 
     # 要約文取得
     summary = response.summary.summary_text.replace(
@@ -356,25 +263,42 @@ def search_sample(
     logger.log_struct(result)
 
     slack_message = format_slack_message(result)
-
     print(slack_message)
 
     return slack_message
 
 
 def format_slack_message(result):
-    # 要約を太文字にする
-    formatted_summary = f"*{result['summary']}*"
+    # 要約を指定された形式にする
+    formatted_summary = f"*要約*: {result['summary']}"
 
-    # 参照をリンク形式にする
+    # 参照をSlackの番号付きリスト形式にする
     formatted_references = []
-    for ref in result['references']:
-        formatted_references.append(f"<{ref['url']}|{ref['title']}>")
+    for idx, ref in enumerate(result['references'], start=1):
+        formatted_references.append(f"{idx}. <{ref['url']}|{ref['title']}>")
 
     # Slackメッセージを組み立てる
-    slack_message = f"{formatted_summary}\n\nReferences:\n" + \
+    slack_message = f"{formatted_summary}\n\n*参照*:\n" + \
         "\n".join(formatted_references)
+
     return slack_message
+
+
+# def format_slack_message(result):
+
+#     # 要約を太文字にする
+#     formatted_summary = f"*{result['summary']}*"
+
+#     # 参照をリンク形式にする
+#     formatted_references = []
+#     for ref in result['references']:
+#         formatted_references.append(f"<{ref['url']}|{ref['title']}>")
+
+#     # Slackメッセージを組み立てる
+#     slack_message = f"{formatted_summary}\n\nReferences:\n" + \
+#         "\n".join(formatted_references)
+
+#     return slack_message
 
 
 def multi_turn_search_sample(
